55b99e2c62fa0c275b10c1525efbdc23b8d53870
Marissa Fandel <mfandel@alumni.nd.edu>
2022-03-16 19:41:55 -0600

    wrote quit function

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const db = mySQL.createConnection(
         database: 'employees_db'
         
     },
-    // console.log(`Connected to database`)
+    console.log(`Welcome to Employee Manager`)
 )
 
 // Call init function to start when node is run
@@ -24,7 +24,7 @@ function init() {
         .prompt(
             {
                 type: "list",
-                message: "Welcome to Employee Manager. What would you like to do?",
+                message: "What would you like to do?",
                 name: "initPrompt",
                 choices: [
                     "View All Current Employees", 
@@ -33,30 +33,60 @@ function init() {
                     "View All Current Roles", 
                     "Add New Role", 
                     "View All Current Departments",
-                    "Add New Department"],
+                    "Add New Department",],
             }
         )
         .then((input) => {
-            if (input.initPrompt === "View All Curent Employees") {
-                db.query('SELECT * FROM employees', function (err, results) {
-                    console.table(results);
-                  });
+            if (input.initPrompt === "View All Current Employees") {
+                viewEmps();
             } else if (input.initPrompt === "Add New Employee") {
                 
             } else if (input.initPrompt === "Update Current Employee Info") {
                 
             } else if (input.initPrompt === "View All Current Roles") {
-                db.query('SELECT * FROM roles', function (err, results) {
-                    console.table(results);
+                db.query('SELECT * FROM roles', function (err, data) {
+                    console.table(data);
                   });
             } else if (input.initPrompt === "Add New Role") {
                 
             } else if (input.initPrompt === "View All Current Departments") {
-                db.query('SELECT * FROM departments', function (err, results) {
-                    console.table(results);
+                db.query('SELECT * FROM departments', function (err, data) {
+                    console.table(data);
                   });
-            } else {
+            } else if (input.initPrompt === "Add New Department") {
                 
+            } else {
+                console.log(`Thank you for using Employee Manager. Goodbye!`);
+                process.exit();
             }
         })
+}
+
+// Function to see if user wants to continue or exit application after each user action
+function quit() {
+    inquirer
+        .prompt(
+            {
+                type: "confirm",
+                message: "Would you like to exit the Employee Manager?",
+                default: false,
+                name: "quitPrompt"
+            }
+        )
+        .then((input) => {
+            if (input.quitPrompt === true) {
+                console.log(`Thank you for using Employee Manager. Goodbye!`);
+                process.exit();                
+            } else {
+                init();
+            }
+        })
+}
+
+// Function to view employee data
+function viewEmps() {
+    db.query('SELECT * FROM employees', (err, data) => {
+        console.table(data);
+        quit();
+      })
 }
